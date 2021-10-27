b6cfb0fe9ecaec3e1c609c87f4cf215ca4087041
renanbuenov <renanbuenov@hotmail.com>
2021-10-27 00:04:21 -0300

    Created mock list with technicians

--- src/app/components/technician/technicians/technicians.component.ts
+++ src/app/components/technician/technicians/technicians.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Technician } from 'src/app/models/technicians';
 
 @Component({
   selector: 'app-technicians',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TechniciansComponent implements OnInit {
 
-  constructor() { }
+  ELEMENT_DATA: Technician[] = [
+    {
+      id: 1,
+      name:  'Renan Bueno',
+      cpf: '086.879.969-69',
+      email: 'renan@hotmail.com',
+      password: '123',
+      profiles: ['0'],
+      dateCreation: '26/10/2021'
+    }
+  ]
+  
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'actions'];
+  dataSource = new MatTableDataSource<Technician>(this.ELEMENT_DATA);
 
+  constructor() { }
+  
   ngOnInit(): void {
+  }
+  
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
