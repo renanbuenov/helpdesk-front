0910f74a0921b45865832f886b3e373a466d2019
renanbuenov <renanbuenov@hotmail.com>
2021-10-28 13:56:10 -0300

    Route for component Technician=Create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AuthGuard } from './auth/auth.guard';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavComponent } from './components/nav/nav.component';
+import { TechnicianCreateComponent } from './components/technician/technician-create/technician-create.component';
 import { TechniciansComponent } from './components/technician/technicians/technicians.component';
 
 const routes: Routes = [
@@ -11,7 +12,8 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: '', component: NavComponent, canActivate: [AuthGuard], children: [
       { path: 'home', component: HomeComponent },
-      { path: 'technicians', component: TechniciansComponent}
+      { path: 'technicians', component: TechniciansComponent},
+      { path: 'technicians/create', component: TechnicianCreateComponent }
     ]
   }
 ];
