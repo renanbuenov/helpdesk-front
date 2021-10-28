6697685d53e2dd508804951dc291202e34c00fbe
renanbuenov <renanbuenov@hotmail.com>
2021-10-27 22:45:43 -0300

    Route for login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 import { NavComponent } from './components/nav/nav.component';
 import { TechniciansComponent } from './components/technician/technicians/technicians.component';
 
 const routes: Routes = [
-  {
-    path: '', component: NavComponent, children: [
+
+  { path: 'login', component: LoginComponent},
+  { path: '', component: NavComponent, children: [
       { path: 'home', component: HomeComponent },
       { path: 'technicians', component: TechniciansComponent}
     ]
