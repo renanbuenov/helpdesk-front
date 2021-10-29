09b936d8e927022d76a5b2d6c339bc1d13e80e2e
renanbuenov <renanbuenov@hotmail.com>
2021-10-28 23:57:15 -0300

    Inteface ticket created

---
+++ src/app/models/ticket.ts
@@ -0,0 +1,13 @@
+export interface Ticket {
+    id?: any;
+    dateOpened?: string;
+    dateClosed?: string;
+    priority: string;
+	status: string;
+	title: string;
+	observation: string;
+    technician: any;
+    client: any;
+    nameClient: string;
+    nameTechnician: string;
+}
