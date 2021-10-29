0371f7b7248976103d62d7beb527d93b01e804d2
renanbuenov <renanbuenov@hotmail.com>
2021-10-29 15:04:16 -0300

    Component ticket-read completed

--- src/app/components/ticket/ticket-read/ticket-read.component.ts
+++ src/app/components/ticket/ticket-read/ticket-read.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Ticket } from 'src/app/models/ticket';
+import { TicketService } from 'src/app/services/ticket.service';
 
 @Component({
   selector: 'app-ticket-read',
@@ -7,9 +11,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicketReadComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ticket: Ticket = {
+    priority:       '',
+    status:         '',
+    title:          '',
+    observation:   '',
+    technician:     '',
+    client:         '',
+    nameClient:     '',
+    nameTechnician: '',
   }
 
+  constructor(
+    private ticketService:     TicketService,
+    private toastService:      ToastrService,
+    private route:             ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    this.ticket.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+  }
+
+  findById(): void {
+    this.ticketService.findById(this.ticket.id).subscribe(answer => {
+      this.ticket = answer;
+    }, ex => {
+      this.toastService.error(ex.error.error);
+    })
+  }  
+
+  returnStatus(status: any): string {
+    if(status == '0') {
+      return 'OPENED'
+    } else if(status == '1') {
+      return 'IN PROGRESS'
+    } else {
+      return 'CLOSED'
+    }
+  }
+
+  returnPriority(priority: any): string {
+    if(status == '0') {
+      return 'LOW'
+    } else if(status == '1') {
+      return 'AVERAGE'
+    } else {
+      return 'HIGH'
+    }
+  }
 }
