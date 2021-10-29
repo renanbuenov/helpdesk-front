8d0d6e446c7ef5d14b02626e47f615068344e136
renanbuenov <renanbuenov@hotmail.com>
2021-10-29 00:01:31 -0300

    Route for tickets-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ import { ClientDeleteComponent } from './components/client/client-delete/client-
 import { ClientUpdateComponent } from './components/client/client-update/client-update.component';
 import { ClientCreateComponent } from './components/client/client-create/client-create.component';
 import { ClientListComponent } from './components/client/client-list/client-list.component';
+import { TicketListComponent } from './components/ticket/ticket-list/ticket-list.component';
 
 const routes: Routes = [
 
@@ -19,15 +20,17 @@ const routes: Routes = [
   { path: '', component: NavComponent, canActivate: [AuthGuard], children: [
       { path: 'home', component: HomeComponent },
 
-      { path: 'technicians', component: TechnicianListComponent },
-      { path: 'technicians/create', component: TechnicianCreateComponent },
+      { path: 'technicians',            component: TechnicianListComponent },
+      { path: 'technicians/create',     component: TechnicianCreateComponent },
       { path: 'technicians/update/:id', component: TechnicianUpdateComponent },
       { path: 'technicians/delete/:id', component: TechnicianDeleteComponent },
 
-      { path: 'clients', component: ClientListComponent },
-      { path: 'clients/create', component: ClientCreateComponent },
-      { path: 'clients/update/:id', component: ClientUpdateComponent },
-      { path: 'clients/delete/:id', component: ClientDeleteComponent }
+      { path: 'clients',                component: ClientListComponent },
+      { path: 'clients/create',         component: ClientCreateComponent },
+      { path: 'clients/update/:id',     component: ClientUpdateComponent },
+      { path: 'clients/delete/:id',     component: ClientDeleteComponent },
+
+      { path: 'tickets',                component: TicketListComponent}
     ]
   }
 ];
