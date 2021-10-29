c40743fc452d2456a79a19a365937cfbe7dd1b66
renanbuenov <renanbuenov@hotmail.com>
2021-10-28 22:39:33 -0300

    Component Technician-Delete created

---
+++ src/app/components/technician/technician-delete/technician-delete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-technician-delete',
+  templateUrl: './technician-delete.component.html',
+  styleUrls: ['./technician-delete.component.css']
+})
+export class TechnicianDeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl : 'https://helpdesk-prod1.herokuapp.com'
+   baseUrl : 'https://helpdesk-prod1.herokuapp.com'
+
 }
