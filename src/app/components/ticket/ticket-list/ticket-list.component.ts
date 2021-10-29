9cd100d1971c9707cda81b2a388d95626eb25b04
renanbuenov <renanbuenov@hotmail.com>
2021-10-29 11:24:49 -0300

    HTML and CSS for ticket component

--- src/app/components/ticket/ticket-list/ticket-list.component.ts
+++ src/app/components/ticket/ticket-list/ticket-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Ticket } from 'src/app/models/ticket';
 
 @Component({
   selector: 'app-ticket-list',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicketListComponent implements OnInit {
 
+  ELEMENT_DATA: Ticket[] = [
+    {
+      id:                             1,
+      dateOpened:          '21/06/2021',
+      dateClosed:          '21/06/2021',
+      priority:                  'ALTA',
+      status:               'ANDAMENTO',
+      title:                'Chamado 1',
+      observation:    'Teste chamado 1',
+      technician:             	  	  1,
+      client:                         6,
+      nameClient:         'Renan Bueno',
+      nameTechnician: 'Albert Einstein',
+    }
+  ]
+  
+  displayedColumns: string[] = ['id', 'title', 'client', 'technician', 'dateOpened', 'priority', 'status', 'actions']
+  dataSource = new MatTableDataSource<Ticket>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
+
+
